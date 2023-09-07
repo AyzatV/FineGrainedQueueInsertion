cddfef725bca4ce3425a0e9e5cb6a7027fe61d8a
AyzatV <129626586+AyzatV@users.noreply.github.com>
2023-09-08 01:01:50 +0300

    Update main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <thread>
 #include "fine_grained_queue.h"
 
 using namespace avhw29;
@@ -9,7 +10,14 @@ int main()
 
 	auto fgq = new FineGrainedQueue(0);
 	// вставка новых 9 узлов
-	for (int i = 1; i < 10; i++) std::cout << fgq->insert(i, i).str() << std::endl;
+	for (int i = 1; i < 10; i++) std::cout << fgq->insert(i, 10).str() << std::endl;
+	
+	// запуск в параллельных потоках удаления и вставки узлов
+	std::thread t1([fgq]() {for (int i = 1; i < 10; i++) std::cout << fgq->remove(i).str() << std::endl; });
+	std::thread t2([fgq]() {for (int i = 10; i < 20; i++) std::cout << fgq->insert(i, 20).str() << std::endl; });
+	
+	t1.join();
+	t2.join();
 	delete fgq;
 
 	system("Pause");
